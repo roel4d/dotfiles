addfee41b78e63631578587f47259abed3f8808f
roel4d <roel4d@webding.org>
2025-06-30 16:28:01 +0200

    LazyVim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,2 @@
-vim.cmd('set runtimepath^=~/.vim runtimepath+=~/.vim/after')
-vim.o.packpath = vim.o.runtimepath
-vim.cmd('source ~/.vimrc')
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
