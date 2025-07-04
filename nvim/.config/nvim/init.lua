2525372d7378c7b849cc89c1b43c806c2fdf8672
roel4d <roel4d@webding.org>
2025-07-04 20:33:55 +0200

    nvim telescope and treesitter

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,11 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader= " "
 
+-- save with ctrl-s and go to normal mode if needed
+vim.keymap.set('n', '<C-s>', ':update<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-s>', '<Esc>:update<CR>', { noremap = true, silent = true })
+
+-- load the lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -11,13 +16,40 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable", 
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- list of plugins
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"}
+}
 local opts = {}
-local plugins = {}
 
+-- load the plugins
 require("lazy").setup(plugins, opts)
+
+-- setup telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader><space>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+
+-- setup treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript", "ruby"},
+  highlight = { enable = true },
+  indent = { enable = true }
+})
+
+-- setup colorscheme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
