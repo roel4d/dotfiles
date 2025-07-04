f6ff1fabff1b90d3b79dc84cdf12af4441733699
roel4d <roel4d@webding.org>
2025-07-04 23:25:15 +0200

    nvim settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -9,8 +9,19 @@ vim.keymap.set('n', '<C-s>', ':update<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-s>', '<Esc>:update<CR>', { noremap = true, silent = true })
 
 -- change window also with tab
-vim.keymap.set('', '<tab>', '<c-w>w', { noremap = true })
-vim.keymap.set('', '<S-tab>', '<c-w>W', { noremap = true })
+vim.keymap.set('n', '<tab>', '<c-w>w', { noremap = true })
+vim.keymap.set('n', '<S-tab>', '<c-w>W', { noremap = true })
+
+-- vertical split
+vim.keymap.set('n', '"', ':vsplit<CR>', { noremap = true })
 
 -- use system clipboard
 vim.opt.clipboard = "unnamedplus"
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+  augroup END
+]]
