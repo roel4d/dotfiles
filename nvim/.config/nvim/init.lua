e08590317d825479f8a639e93bdc8ad23d67f6f1
roel4d <roel4d@webding.org>
2025-07-04 21:24:24 +0200

    nvim split into multiple files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
-
--- save with ctrl-s and go to normal mode if needed
-vim.keymap.set('n', '<C-s>', ':update<CR>', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-s>', '<Esc>:update<CR>', { noremap = true, silent = true })
-
 -- load the lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -22,47 +12,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- list of plugins
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build= ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
-}
-local opts = {}
+-- custom vim settings
+require("settings")
 
 -- load the plugins
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
--- setup telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader><space>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-
--- setup treesitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript", "ruby"},
-  highlight = { enable = true },
-  indent = { enable = true }
-})
-
--- setup neotree
-vim.keymap.set('n', '\\', ':Neotree reveal=true position=float toggle=true<CR>')
-
--- setup colorscheme
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,10 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader= " "
+
+-- save with ctrl-s and go to normal mode if needed
+vim.keymap.set('n', '<C-s>', ':update<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-s>', '<Esc>:update<CR>', { noremap = true, silent = true })
+
